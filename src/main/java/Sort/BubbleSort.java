be87d7d80c67ac9141b92815045e940ca6650e7d
106540 <niyati.venugopal@thoughtfocous.com>
2022-02-17 14:17:04 +0530

    Updated Commit

--- src/main/java/Sort/BubbleSort.java
+++ src/main/java/Sort/BubbleSort.java
@@ -5,23 +5,14 @@ package Sort;
 import java.util.Scanner;
 
     public class BubbleSort {
-        BubbleSort(){
-            Scanner sc=new Scanner(System.in);
-            System.out.print("Enter array length");
-            int n=sc.nextInt();
-            int [] arr=new int[n];
-
-            for(int i=0;i<arr.length;i++){
-                System.out.print("Enter array elements") ;
-                arr[i]=sc.nextInt();
-            }
+        public void sorting(int arr[]) {
             System.out.println("Before Sorting ");
-            for(int i=0;i<arr.length;i++) {
+            for (int i = 0; i < arr.length; i++) {
                 System.out.println(arr[i]);
             }
             System.out.println();
             int help;
-            for(int i=0;i<arr.length-1;i++) {
+            for (int i = 0; i < arr.length - 1; i++) {
                 for (int j = 0; j < arr.length - 1 - i; j++) {
                     if (arr[j] > arr[j + 1]) {
                         help = arr[j];
@@ -30,13 +21,10 @@ import java.util.Scanner;
                     }
                 }
             }
-
             System.out.println("After Sorting ");
-            for(int i=0;i<arr.length;i++) {
+            for (int i = 0; i < arr.length; i++) {
                 System.out.println(arr[i]);
             }
-
-
         }
     }
 
--- src/main/java/Sort/Sorting.java
+++ src/main/java/Sort/Sorting.java
@@ -1,7 +1,19 @@
 package Sort;
 
+import java.util.Scanner;
+
 public class Sorting {
     public static void main(String args[]) {
         BubbleSort bs = new BubbleSort();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array length");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter array elements");
+            arr[i] = sc.nextInt();
+        }
+        bs.sorting( arr);
     }
 }
