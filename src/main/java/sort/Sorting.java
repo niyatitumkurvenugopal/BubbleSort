d3e2309f8303430352881469383977e69bd49430
106540 <niyati.venugopal@thoughtfocous.com>
2022-02-17 14:48:20 +0530

    Updated Commit

--- src/main/java/sort/Sorting.java
+++ src/main/java/sort/Sorting.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Sorting {
     public static void main(String args[]) {
-        BubbleSort bs = new BubbleSort();
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter array length");
         int n = sc.nextInt();
         int[] arr = new int[n];
-
         for (int i = 0; i < arr.length; i++) {
             System.out.print("Enter array elements");
             arr[i] = sc.nextInt();
         }
+
+        BubbleSort bs = new BubbleSort();
         bs.sorting( arr);
     }
 }
